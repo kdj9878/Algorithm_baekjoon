d907d5d39b05de4520107233a3ea071b0d1e52be
kdj9878 <kdj9878@gmail.com>
2022-07-07 10:54:52 +0900

    #feat - Sprout

---
+++ src/main/java/ioArithmeticOperation/Sprout.java
@@ -0,0 +1,26 @@
+package ioArithmeticOperation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sprout {
+
+    /*문제
+    아래 예제와 같이 새싹을 출력하시오.
+
+            입력
+    입력은 없다.
+
+    출력
+    새싹을 출력한다.*/
+
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
