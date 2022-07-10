822fcad5a2ca3a5240f3b6198df11d6a2682f915
kdj9878 <kdj9878@gmail.com>
2022-07-10 12:55:45 +0900

    #feat - AplusBminusFour, PlusCycle

---
+++ src/main/java/loop/PlusCycle.java
@@ -0,0 +1,59 @@
+package loop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PlusCycle {
+    /*문제
+    0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다.
+    먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다. 그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다. 다음 예를 보자.
+
+    26부터 시작한다. 2+6 = 8이다. 새로운 수는 68이다. 6+8 = 14이다. 새로운 수는 84이다. 8+4 = 12이다. 새로운 수는 42이다. 4+2 = 6이다. 새로운 수는 26이다.
+
+    위의 예는 4번만에 원래 수로 돌아올 수 있다. 따라서 26의 사이클의 길이는 4이다.
+
+    N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 N이 주어진다. N은 0보다 크거나 같고, 99보다 작거나 같은 정수이다.
+
+    출력
+    첫째 줄에 N의 사이클 길이를 출력한다.*/
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String[] strArray = new String[2];
+        int intValue = Integer.parseInt(str);
+        if(str.length() < 2) {
+            strArray[0] = "0";
+            strArray[1] = str;
+        }
+        else {
+            strArray[0] = str.substring(0, 1);
+            strArray[1] = str.substring(1, 2);
+        }
+
+        int count = 0;
+        while (true) {
+
+            int val1 = Integer.parseInt(strArray[0]);
+            int val2 = Integer.parseInt(strArray[1]);
+            String sumValue = String.valueOf(val1 + val2);
+            if(sumValue.length() > 1) {
+                sumValue = sumValue.substring(1, 2);
+            }
+            strArray[0] = String.valueOf(val2);
+            strArray[1] = sumValue;
+            int result = Integer.parseInt((strArray[0] + strArray[1]));
+
+            count++;
+            if(intValue == result) {
+                System.out.println(count);
+                break;
+            }
+
+        }
+    }
+}
