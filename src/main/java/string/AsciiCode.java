0b232259143be9157e2e313e172d1e924b5d1762
kdj9878 <kdj9878@gmail.com>
2022-07-18 19:08:58 +0900

    #feat - AsciiCode

---
+++ src/main/java/string/AsciiCode.java
@@ -0,0 +1,25 @@
+package string;
+
+import java.io.*;
+
+public class AsciiCode {
+
+    /*문제
+    알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+    입력
+    알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+
+    출력
+    입력으로 주어진 글자의 아스키 코드 값을 출력한다.*/
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int cha = (int)br.readLine().charAt(0);
+        br.close();
+        bw.write(cha + "");
+        bw.flush();
+        bw.close();
+    }
+}
